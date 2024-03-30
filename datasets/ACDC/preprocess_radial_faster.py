a94b18e12c3336fbe4464ded54d1f9c4c7cbf0a3
Niraj@phindy <nimahajan@ucsd.edu>
2024-03-29 17:06:14 -0700

    preprocess written with interp - saving massive data

--- datasets/ACDC/preprocess_radial_faster.py
+++ datasets/ACDC/preprocess_radial_faster.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append('/root/Cardiac-MRI-Reconstrucion/')
 from tqdm import tqdm
 import kornia
+import pickle
 from kornia.metrics import ssim
 from torchvision import transforms
 import matplotlib.pyplot as plt
@@ -85,8 +86,8 @@ N_COILS = 8
 LOOP_FRAMES = 120
 
 kb_ob = tkbn.KbNufft(im_size=(512,512), grid_size = (1024,1024), numpoints = 4, kbwidth = 2.34, device = device)
-adjkb_ob = tkbn.KbNufftAdjoint(im_size=(256,256), grid_size = (1024,1024), numpoints = 4, kbwidth = 2.34, device = device)
-kbinterp = tkbn.KbInterpAdjoint(im_size=(256,256), grid_size = (256,256), numpoints = 2, kbwidth = 3.34, device = device)
+kbinterp = tkbn.KbInterpAdjoint(im_size=(256,256), grid_size = (256,256), numpoints = 3, kbwidth = 2.34, device = device)
+kbinterp2 = tkbn.KbInterpAdjoint(im_size=(256,256), grid_size = (1024,1024), numpoints = 8, kbwidth = 0.84, device = device)
 
 def get_omega2d(thetas, rad_max = 128):
     
@@ -129,12 +130,16 @@ else:
     metadic['frames_per_vid_per_patient'] = [0 for i in range(NUM_PATIENTS)]
     
 
-    metadic['spoke_masks'] = torch.zeros(377,RES,RES) == 0
-    for angle_index in tqdm(range(377), desc = 'Writing Metadata spoke_masks'):
-        angles = GA*(np.arange(1)+angle_index)
-        omega1 = get_omega2d(angles, rad_max = 256).to(device).reshape(2,-1)
-        dcomp_full = tkbn.calc_density_compensation_function(ktraj=omega1, im_size=(256,256), grid_size = (1024,1024), numpoints = 4, kbwidth = 2.34)
-        metadic['spoke_masks'][angle_index] = torch.fft.fftshift(kbinterp(dcomp_full, omega1)[0,:].abs(), dim = (-2,-1)).cpu() > 0
+    # metadic['spoke_masks'] = torch.zeros(377,RES,RES) == 0
+    # for angle_index in tqdm(range(377), desc = 'Writing Metadata spoke_masks'):
+    #     angles = GA*(np.arange(1)+angle_index)
+    #     omega1 = get_omega2d(angles, rad_max = 256).to(device).reshape(2,-1)
+    #     dcomp_full = tkbn.calc_density_compensation_function(ktraj=omega1, im_size=(256,256), grid_size = (1024,1024), numpoints = 8, kbwidth = 0.34)
+    #     metadic['spoke_masks'][angle_index] = torch.fft.fftshift(kbinterp(dcomp_full, omega1)[0,:].abs().squeeze(), dim = (-2,-1)).cpu() > 0
+    #     mask = torch.fft.fftshift(kbinterp(dcomp_full, omega1)[0,:].abs().squeeze(), dim = (-2,-1)).cpu()
+    #     print(mask.sum())
+    #     plt.imsave('mask_8.jpg', mask > 0)
+    #     asdf
 
 
     for p_num in tqdm(range(NUM_PATIENTS), desc = 'Writing Metadata patient data'):
@@ -165,12 +170,19 @@ if not args.metadata_only:
         os.makedirs(folder_name, exist_ok = True)
 
         path = num_to_str(p_num+1)
-        d, flag, dvid = read_nib_preprocess(path)
+        d_inp, flag, dvid = read_nib_preprocess(path)
         if flag:
             print("Patient {} flagged, {} video(s) dropped".format(p_num, dvid))
-        d = torch.permute(torch.FloatTensor(d).unsqueeze(4), (2,3,4,0,1))
-        num_vids, num_frames, n_coils, n_row, n_col = d.shape
-        d = transform(d.reshape(num_vids*num_frames, 1, n_row, n_col)).reshape(num_vids, num_frames, n_coils, RES, RES)
+        d_inp = torch.permute(torch.FloatTensor(d_inp).unsqueeze(4), (2,3,4,0,1))
+        num_vids, num_frames, n_coils, n_row, n_col = d_inp.shape
+        d_padded = torch.zeros(num_vids, num_frames, n_coils, max(n_row, n_col),max(n_row, n_col))
+        row_pad = (max(n_row, n_col) - n_row)//2
+        col_pad = (max(n_row, n_col) - n_col)//2
+
+        d_padded[:,:,:,row_pad:row_pad + n_row, col_pad:col_pad+n_col] = d_inp
+        del d_inp
+
+        d = transform(d_padded.reshape(num_vids*num_frames, 1, max(n_row,n_col), max(n_row,n_col))).reshape(num_vids, num_frames, n_coils, RES, RES)
         d = d - d.min(-1)[0].min(-1)[0].unsqueeze(-1).unsqueeze(-1)
         d = d / (1e-10 + d.max(-1)[0].max(-1)[0].unsqueeze(-1).unsqueeze(-1))
 
@@ -178,7 +190,10 @@ if not args.metadata_only:
         for vi in tqdm(range(num_vids), desc = 'Running for Patient {}'.format(p_num+1)):
             dic = {}
             dic['targ_video'] = d[vi,np.arange(LOOP_FRAMES)%num_frames] # FRAMES, 1, RES, RES
-            dic['coilwise_input'] = torch.zeros((LOOP_FRAMES, N_COILS, RES, RES)).type(torch.uint8) # FRAMES, COILS, RES, RES
+            dic['spoke_mask'] = torch.zeros((LOOP_FRAMES, 1, RES, RES)).type(torch.uint8) # FRAMES, COILS, RES, RES
+            # dic['coilwise_input_mag'] = torch.zeros((LOOP_FRAMES, N_COILS, RES, RES)).type(torch.uint8) # FRAMES, COILS, RES, RES
+            dic['coilwise_input'] = torch.zeros((LOOP_FRAMES, N_COILS, RES, RES)) # FRAMES, COILS, RES, RES
+            dic['coilwise_input'] = torch.complex(dic['coilwise_input'],dic['coilwise_input'])
 
             for fi in range(LOOP_FRAMES):
                 coil_variant = metadic['coil_variant_per_patient_per_video'][p_num][vi]
@@ -196,13 +211,23 @@ if not args.metadata_only:
                 y = kb_ob(torch.complex(padded_frame,padded_frame*0).to(device),omega1)
 
                 dcomp_full = tkbn.calc_density_compensation_function(ktraj=omega1, im_size=(256,256), grid_size = (1024,1024), numpoints = 4, kbwidth = 2.34).to(device)
-                
-                inverse = adjkb_ob(dcomp_full*y, omega1)[0,:].abs().cpu()
-                
-                inverse = inverse - inverse.min(-1)[0].min(-1)[0].unsqueeze(-1).unsqueeze(-1)
-                inverse = inverse / (1e-10 + inverse.max(-1)[0].max(-1)[0].unsqueeze(-1).unsqueeze(-1))
-                
-                dic['coilwise_input'][fi,:,:,:] = (inverse*255).type(torch.uint8)
+                dic['spoke_mask'][fi,0,:,:] = torch.fft.fftshift(kbinterp(dcomp_full, omega1)[0,:].abs().squeeze(), dim = (-2,-1)).cpu() > 0
+
+                myfft_interp = torch.fft.fftshift(kbinterp2(dcomp_full*y, omega1)[0,:].cpu())
+                myfft_interp = myfft_interp[:,::4,::4]
+
+                dic['coilwise_input'][fi] = myfft_interp
+
+                # inverse_interp = torch.fft.ifft2(torch.fft.ifftshift(myfft_interp))
+
+                # for coil in range(8):
+                #     plt.imsave('coil_{}_inverse_interp.jpg'.format(coil), inverse_interp.abs()[coil], cmap = 'gray')
+                #     plt.imsave('coil_{}_mag_of_log_interp.jpg'.format(coil), (myfft_interp + torch.complex(torch.tensor([1e-10]),torch.tensor([1e-10])).exp()).log().abs()[coil], cmap = 'gray')
+                #     plt.imsave('coil_{}_phase_interp.jpg'.format(coil), torch.atan2(myfft_interp.imag, myfft_interp.real)[coil], cmap = 'gray')
+                #     plt.imsave('coil_{}_mask.jpg'.format(coil), dic['spoke_mask'][fi,0], cmap = 'gray')
+
+                # if fi == 1:
+                #     asdf
 
             dic['targ_video'] = (dic['targ_video']*255).type(torch.uint8)
             torch.save(dic, os.path.join(folder_name, 'vid_{}.pth'.format(vi)))
